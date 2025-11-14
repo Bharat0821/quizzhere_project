8b6830cf8c5bb8259cf1a04671ca864ec1bae622
Bharat0821 <swamibharat2110@gmail.com>
2025-11-14 12:30:46 +0530

    update

---
+++ server/api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import connectDB from "../database/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+// Routes
+import userRoute from "../routes/user.route.js";
+import quizRoute from "../routes/quiz.route.js";
+import questionRoute from "../routes/question.route.js";
+
+configDotenv();
+await connectDB(); // IMPORTANT for serverless
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-frontend.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
+// API Routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/quiz", quizRoute);
+app.use("/api/v1/question", questionRoute);
+
+// Export Express App for Vercel
+export default app;
